67589ed4bc8c80827542723088a616d07aa3f35c
Bradley Evans <bradleyt.evans@gmail.com>
2022-12-14 17:24:15 -0500

    lesson 13: PHP Partials

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
-<?php
+<?php 
 
-$books = [
-    [
-        'name' => 'Do androids dream of electric sheep?',
-        'author' => 'Phillip Dick',
-        'purchaseUrl' => 'example.com',
-        'releaseDate' => '1968'
-    ],
-    [
-        'name' => 'Project Hail Mary',
-        'author' => 'Andy Weir',
-        'purchaseUrl' => 'example.com',
-        'releaseDate' => '2021'
-    ],
-    [
-        'name' => 'The  Martian',
-        'author' => 'Andy Weir',
-        'purchaseUrl' => 'example.com',
-        'releaseDate' => '2011'
-    ]
-];
+$heading = "Home";
 
-
-$filteredBooks = array_filter($books, function ($book) {
-    return $book['releaseDate'] >= 1950 && $book['releaseDate'] <= 2000;
-});
-
-require 'index.view.php';
+require "views/index.view.php";
