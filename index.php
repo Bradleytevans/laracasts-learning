9fe50e304a2e0962488915386877c06f26f4321d
Bradley Evans <bradleyt.evans@gmail.com>
2022-12-14 13:18:01 -0500

    lesson 10

--- index.php
+++ index.php
@@ -1,49 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-    <?php
-    $books = [
-        [
-            'name' => 'Do androids dream of electric sheep?',
-            'author' => 'Phillip Dick',
-            'purchaseUrl' => 'example.com',
-            'releaseDate' => '1968'
-        ],
-        [
-            'name' => 'Project Hail Mary',
-            'author' => 'Andy Weir',
-            'purchaseUrl' => 'example.com',
-            'releaseDate' => '2021'
-        ],
-        [
-            'name' => 'The  Martian',
-            'author' => 'Andy Weir',
-            'purchaseUrl' => 'example.com',
-            'releaseDate' => '2011'
-        ]
-    ];
+$books = [
+    [
+        'name' => 'Do androids dream of electric sheep?',
+        'author' => 'Phillip Dick',
+        'purchaseUrl' => 'example.com',
+        'releaseDate' => '1968'
+    ],
+    [
+        'name' => 'Project Hail Mary',
+        'author' => 'Andy Weir',
+        'purchaseUrl' => 'example.com',
+        'releaseDate' => '2021'
+    ],
+    [
+        'name' => 'The  Martian',
+        'author' => 'Andy Weir',
+        'purchaseUrl' => 'example.com',
+        'releaseDate' => '2011'
+    ]
+];
 
 
-    $filteredBooks = array_filter($books, function($book) {
-        return $book['releaseDate'] >= 1950 && $book['releaseDate'] <= 2000;
-    });
-    ?>
-    <ul>
-        <?php foreach ($filteredBooks as $book) : ?>
-            <li>
-                <a href="<?= $book['purchaseUrl'] ?>"><?= $book['name'] . ' : ' . 'Publised ' . $book['releaseDate']; ?></a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</body>
+$filteredBooks = array_filter($books, function ($book) {
+    return $book['releaseDate'] >= 1950 && $book['releaseDate'] <= 2000;
+});
 
-</html>
+require 'index.view.php';
