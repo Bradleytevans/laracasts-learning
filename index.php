11c7b5b95cae9d4871f22f49ea69de084d3c98bf
Bradley Evans <bradleyt.evans@gmail.com>
2022-12-13 22:33:41 -0500

    episode 6

--- index.php
+++ index.php
@@ -1,38 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
-        }
-    </style>
 </head>
-
 <body>
+    <h1>Recommended Books</h1>
 
     <?php 
-        $name = "Dark Matter";
-        $read = false;
+        $books = [
+            "do androids dream of electric sheep?",
+            "the langoliers",
+            "hail mary"
+        ];
     ?>
-    <?php 
-        if($read) {
-            $message = "you have read $name";
-        } else {
-            $message = "you have not read $name";
-        }
-    ?>
-    <h1>
-        <?= $message ?>
-    </h1>
+
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <li><?= $book ?></li>
+        <?php endforeach; ?>
+    </ul>
 </body>
-
 </html>
