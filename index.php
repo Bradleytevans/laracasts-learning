3e7d2f7d4503930923c698fcf9883ba3d6e56686
Bradley Evans <bradleyt.evans@gmail.com>
2022-12-14 08:10:11 -0500

    lesson 7

--- index.php
+++ index.php
@@ -1,26 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>Recommended Books</h1>
 
-    <?php 
-        $books = [
-            "do androids dream of electric sheep?",
-            "the langoliers",
-            "hail mary"
-        ];
+    <?php
+    $books = [
+        [
+            'name' => 'Do androids dream of electric sheep?',
+            'author' => 'Phillip Dick',
+            'purchaseUrl' => 'example.com'
+        ],
+        [
+            'name' => 'Project Hail Mary',
+            'author' => 'Andy Weir',
+            'purchaseUrl' => 'example.com'
+        ]
+    ];
     ?>
-
     <ul>
         <?php foreach ($books as $book) : ?>
-            <li><?= $book ?></li>
+            <a href="<?= $book['purchaseUrl'] ?>">  <li><?= $book['name'] . ' : ' . $book['author']; ?></li> </a>
         <?php endforeach; ?>
     </ul>
 </body>
+
 </html>
