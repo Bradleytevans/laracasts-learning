f7b3ce381700ebd229f40f7442b8c2f6766d904a
Bradley Evans <bradleyt.evans@gmail.com>
2022-12-14 09:17:39 -0500

    lesson 8

--- index.php
+++ index.php
@@ -15,18 +15,41 @@
         [
             'name' => 'Do androids dream of electric sheep?',
             'author' => 'Phillip Dick',
-            'purchaseUrl' => 'example.com'
+            'purchaseUrl' => 'example.com',
+            'releaseDate' => '1968'
         ],
         [
             'name' => 'Project Hail Mary',
             'author' => 'Andy Weir',
-            'purchaseUrl' => 'example.com'
+            'purchaseUrl' => 'example.com',
+            'releaseDate' => '2021'
+        ],
+        [
+            'name' => 'The  Martian',
+            'author' => 'Andy Weir',
+            'purchaseUrl' => 'example.com',
+            'releaseDate' => '2011'
         ]
     ];
+
+    function filterByAuthor($books, $author)
+    {
+        $filteredBooks = [];
+        foreach ($books as $book) {
+            if ($book['author'] === $author) {
+                $filteredBooks[] = $book;
+            }
+        }
+
+        return $filteredBooks;
+    }
+
     ?>
     <ul>
-        <?php foreach ($books as $book) : ?>
-            <a href="<?= $book['purchaseUrl'] ?>">  <li><?= $book['name'] . ' : ' . $book['author']; ?></li> </a>
+        <?php foreach (filterByAuthor($books, 'Phillip Dick') as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>"><?= $book['name'] . ' : ' . 'Publised ' . $book['releaseDate']; ?></a>
+            </li>
         <?php endforeach; ?>
     </ul>
 </body>
