564f32ce9f488568c1e96d64748021693f5c1ee8
Bradley Evans <bradleyt.evans@gmail.com>
2022-12-13 22:02:22 -0500

    finished episode 5

--- index.php
+++ index.php
@@ -1,18 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
+
 <body>
+
+    <?php 
+        $name = "Dark Matter";
+        $read = false;
+    ?>
+    <?php 
+        if($read) {
+            $message = "you have read $name";
+        } else {
+            $message = "you have not read $name";
+        }
+    ?>
     <h1>
-        <?php 
-        $greeting = "Hello";
-        echo $greeting . " " . 'World';
-        echo "$greeting World!"
-        ?>
+        <?= $message ?>
     </h1>
 </body>
+
 </html>
