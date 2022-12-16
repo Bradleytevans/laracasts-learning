507a482d589fa692c8e808dcc9feb417857acbf5
Bradley Evans <bradleyt.evans@gmail.com>
2022-12-16 16:59:50 -0500

    lesson 17: PDO First Steps

--- functions.php
+++ functions.php
@@ -10,20 +10,6 @@ function urlIs($value) {
     return $_SERVER['REQUEST_URI'] === $value;
 }
 
-function routeToController($uri, $routes) {
-    if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
-    } else {
-        abort();
-    }
-}
 
-function abort($code = 404) {
-    http_response_code($code);
-    
-    require "views/{$code}.php";
-
-    die();
-}
 
 
