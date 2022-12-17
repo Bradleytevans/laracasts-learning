5d6c7c4a0a47b47e0bc2c782b5b6f44b092690dc
Bradley Evans <bradleyt.evans@gmail.com>
2022-12-16 19:26:29 -0500

    Lesson 18: extract a PHP database class

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
 require 'functions.php';
-// require 'router.php';
+require 'router.php';
+require 'Database.php';
 
 
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
-$pdo = new PDO($dsn, 'root');
+$db = new Database(); 
 
-$statement = $pdo->prepare("select * from posts");
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
