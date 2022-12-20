b3dfbc1db3b5767cf01ff79181cf96aaa14ca9d8
Bradley Evans <bradleyt.evans@gmail.com>
2022-12-19 19:50:03 -0500

    lesson 26: always escape untrusted input

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -5,7 +5,11 @@ $db = new Database($config['database']);
 
 $heading = "Create A Note";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+    $db->query('INSERT INTO notes (body, user_id) VALUES
+    (:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1 
+    ]);
 }
 
 require 'views/note-create.view.php';
